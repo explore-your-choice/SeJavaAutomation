f8370222ecc0c1e34e334708dd92b47889730502
rsharma96 <30986019+rsharma96@users.noreply.github.com>
2017-09-20 02:46:27 +0530

    Added screenshot class to utility Package

---
+++ SeleniumJ/src/com/github/utility/ScreenshotCapture.java
@@ -0,0 +1,14 @@
+package com.github.utility;
+
+/**
+ * Created by gaurnitai on 2017-09-20.
+ */
+public class ScreenshotCapture {
+
+    public void takeScreenshot(){
+
+        System.out.println("Screenshot taken");
+
+    }
+
+}
