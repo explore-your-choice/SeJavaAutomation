46c1cb2c9b1d2f3042b3c6f81bd940b184d89d99
rsharma96 <30986019+rsharma96@users.noreply.github.com>
2017-09-20 02:18:11 +0530

    Added OpenBrowser class to Selenium Package

---
+++ SeleniumJ/src/com/github/selenium/OpenChrome.java
@@ -0,0 +1,15 @@
+package com.github.selenium;
+
+/**
+ * Created by rsharma96 on 2017-09-20.
+ */
+public class OpenChrome {
+
+    public static void main(String args[]) {
+
+        System.out.println("Chrome browser is opening");
+
+
+    }
+
+}
